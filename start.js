b679b806bbbb6a66b82ccce369adaf308d1191d5
Khash Sajadi <khash@cloud66.com>
2016-01-25 18:14:52 +0000

    remove comments

--- start.js
+++ start.js
@@ -21,7 +21,6 @@ http.createServer(function (req, res) {
       color = 'off'
     }
 
-//{"build":{"build_url":"https://codeship.com/projects/128515/builds/11512884","commit_url":"https://bitbucket.org/cloud66/maestro/commits/ed02f309b67586c5957afdb815de315fba9d88a3","project_id":128515,"build_id":11512884,"status":"testing","project_name":"cloud66/maestro","project_full_name":"cloud66/maestro","commit_id":"ed02f309b67586c5957afdb815de315fba9d88a3","short_commit_id":"ed02f","message":"bugfix: step 6, pass correct cloud\n","c
     request.post(
         process.env.MAKER_ENDPOINT,
         { json: { "value1" : color }},
