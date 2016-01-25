2778e16bb27260b8697e8fc604717199e0236392
Khash Sajadi <khash@cloud66.com>
2016-01-25 18:29:58 +0000

    filter by branch

--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var request = require('request');
 
+console.log("Started on 8080")
+
 http.createServer(function (req, res) {
   var load = "";
   req.on('data', function (chunk) {
@@ -10,29 +12,34 @@ http.createServer(function (req, res) {
     console.log("Received")
     console.log(load)
     data = JSON.parse(load);
-    payload = data['build']
-    if (payload['status'] == 'testing') {
-      color = 'blue'
-    } else if (payload['status'] == 'error') {
-      color = 'red'
-    } else if (payload['status'] == 'success') {
-      color = 'green'
-    } else {
-      color = 'off'
-    }
 
-    request.post(
-        process.env.MAKER_ENDPOINT,
-        { json: { "value1" : color }},
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-              console.log("Posted " + color)
-              console.log(body)
-            } else {
-              console.log(error)
-            }
-        }
-    );
+    payload = data['build']
+
+    // filter by branch
+    if (payload['branch'] == 'dev') {
+      if (payload['status'] == 'testing') {
+        color = 'blue'
+      } else if (payload['status'] == 'error') {
+        color = 'red'
+      } else if (payload['status'] == 'success') {
+        color = 'green'
+      } else {
+        color = 'off'
+      }
+
+      request.post(
+          process.env.MAKER_ENDPOINT,
+          { json: { "value1" : color }},
+          function (error, response, body) {
+              if (!error && response.statusCode == 200) {
+                console.log("Posted " + color)
+                console.log(body)
+              } else {
+                console.log(error)
+              }
+          }
+      );
+    }
 
     res.writeHead(200);
     res.end("");
