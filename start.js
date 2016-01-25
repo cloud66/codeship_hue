71b9d4cbd78621aca7ea82b1c7d21019967501e0
Khash Sajadi <khash@cloud66.com>
2016-01-25 18:07:13 +0000

    correct payload

--- start.js
+++ start.js
@@ -10,22 +10,24 @@ http.createServer(function (req, res) {
     console.log("Received")
     console.log(load)
     data = JSON.parse(load);
-    if (data['status'] == 'testing') {
+    payload = data['build']
+    if (payload['status'] == 'testing') {
       color = 'blue'
-    } else if (data['status'] == 'error') {
+    } else if (payload['status'] == 'error') {
       color = 'red'
-    } else if (data['status'] == 'success') {
+    } else if (payload['status'] == 'success') {
       color = 'green'
     } else {
       color = 'off'
     }
 
+//{"build":{"build_url":"https://codeship.com/projects/128515/builds/11512884","commit_url":"https://bitbucket.org/cloud66/maestro/commits/ed02f309b67586c5957afdb815de315fba9d88a3","project_id":128515,"build_id":11512884,"status":"testing","project_name":"cloud66/maestro","project_full_name":"cloud66/maestro","commit_id":"ed02f309b67586c5957afdb815de315fba9d88a3","short_commit_id":"ed02f","message":"bugfix: step 6, pass correct cloud\n","c
     request.post(
         process.env.MAKER_ENDPOINT,
         { json: { "value1" : color }},
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
-              console.log("Posted" + color)
+              console.log("Posted " + color)
               console.log(body)
             } else {
               console.log(error)
